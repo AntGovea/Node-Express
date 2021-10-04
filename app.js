1f0f2c296906edcaa81cd0c35a47289f35ce35fb
Antonio Govea <79109840+AntGovea@users.noreply.github.com>
2021-10-04 16:10:15 -0500

    ...

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-// const http = require("http");
-// const puerto = 4000;
+//CONFIGURACION DEL SISTEMA E IMPORTACIONES NEESARIAS
+//AUTOR:JOSE ANTONIO GOVEA MILLAN 
+//DESCRIPCION:CONFIGURACION BASICA DE SERVIDOR CON NODE Y EXPRESS 
 
-// http
-// 	.createServer((req, res) => {
-// 		res.write("Inicilizando servidor");
-// 		res.end();
-// 	})
-// 	.listen(puerto);
+require("dotenv").config();
+//IMPORTACION DE LIBRERIA DE EXPRESS
+const express = require("express");
+const app = express();
+//IMPORTACION DE HANDLEBEARS
+const hbs = require('hbs');
+//ASIGNACION DE PUERTO
+const port = process.env.PORT  | 4000 ;
 
-// console.log("Servidor corriendo en puerto " + puerto);
+//ASIGNANDO LA CARPETA PUBLICA DE MANERA ESTATICA
+app.use(express.static('public'));
 
 
+//RUTAS DE SERVIDOR
 
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/public/index.html");
+	
+	res.send("Respuesta de ruta Home");
+	console.log("Ruta Home ");
+});
+
+
+//Ruta por default
+app.get('*', (req, res) => {
+	// res.sendFile(__dirname + "/public/404.html");
+	// console.log("Ruta no encontrada");
+	res.sendFile(__dirname + "/public/index.html");
+	
+	res.send("Respuesta de ruta Home");
+	console.log("Ruta Home ");
+});
+
+//Informacion de Puerto
+app.listen(port);
+console.log("Servidor corriendo en puerto " + port); 
---
+++ models/server.js
@@ -0,0 +1,5 @@
+
+//Middlewares
+
+
+//Rutas de aplicacion
